bc6c76888ceb85cfc8eff6b814e5d2f7cccf8749
Toshifumi Kurosawa <kuromailserver@gmail.com>
2019-10-31 01:49:19 +0900

    WIP

--- database/db.js
+++ database/db.js
@@ -1,7 +1,8 @@
 const Sequelize = require("sequelize");
 const db = {};
-const sequelize = new Sequelize("nodejs_tasks", "root1", "", {
+const sequelize = new Sequelize("nodejs_tasks", "root", "root", {
   host: "localhost",
+  port: 8889,
   dialect: "mysql",
   operatorsAliases: false,
 
